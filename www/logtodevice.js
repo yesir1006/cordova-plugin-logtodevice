84bd5fa76f22587a5e61174967f1b51c5e3a35dd
Vitto <admin@vitto.io>
2018-05-22 00:30:56 +0200

    minor changes

--- www/logtodevice.js
+++ www/logtodevice.js
@@ -1,15 +1,44 @@
 var exec = require ('cordova/exec');
 
+var _p = 'LogToDevice';
+var _pf = 'log';
+
 var logtodevice = {};
-logtodevice.log = function (obj) {
-  if (typeof (obj) !== 'string') {
-    try {
-      obj = JSON.stringify (obj);
-    } catch (ex) {
-      console.error (ex);
-    }
+
+var toString = function (obj, extend) {
+  // add logic to extend obj later
+  var s = '';
+  try {
+    s = JSON.stringify (obj);
+  } catch (e) {
+    console.error (e);
   }
-  exec (null, null, 'LogToDevice', 'log', [obj]);
+  return s;
+};
+
+logtodevice.log = function (obj, extend) {
+  if (typeof (extend) === 'undefined' || extend == null) {
+  	extend = false;
+  }
+  if (typeof (obj) !== 'string') {
+    obj = toString (obj, extend);
+  }
+  exec (null, null, _p, _pf, [obj]);
+};
+
+logtodevice.override = function (c) {
+  var oc = function () {};
+  if (typeof (c.log) === 'function') {
+  	oc = c.log;
+  }
+  c.log = function () {
+    var a = [];
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      a.push (toString (arguments [i]));
+    }
+    exec (null, null, _p, _pf, [a.join (' ')]);
+    oc.apply (null, a);
+  };
 };
 
 module.exports = logtodevice;
